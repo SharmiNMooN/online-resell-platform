14aba8e4eea8d80fefd6d4662991c31d460c458e
Sharmin Moon <sharminmoon0308@gmail.com>
2022-11-27 23:19:05 +0600

    add extra section contact form in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 import { useLoaderData } from "react-router-dom";
-import { Col, Row } from "react-bootstrap";
+import {Col, FloatingLabel, Form, Image, Row} from "react-bootstrap";
 
 import axios from "axios";
 
 import Slider from "../shared/Slider/Slider";
 import Category from "../Category/Category";
 import AdvertiesProduct from "../AdvertiesProduct/AdvertiesProduct";
+import Button from "react-bootstrap/Button";
 
 const Home = () => {
   document.title = "Home";
@@ -74,6 +75,67 @@ const Home = () => {
                   </Col>
                 ))
               : ""}
+          </Row>
+        </Col>
+
+        <Col sx={12} sm={12} md={12} lg={12} className="mt-4 bg-light text-dark">
+          <h2 className="text-primary text-center text-decoration-underline"> Contact Us</h2>
+          <Row className="mt-4">
+            <Col className=" mb-2 d-none d-sm-block" sx={12} sm={12} md={12} lg={12}>
+              <div className="text-center">
+                <Image
+                    height={200}
+                    width={300}
+                    roundedCircle
+                    src="https://media.istockphoto.com/id/1312566254/photo/contact-us-concept-icon-telephone-address-and-email-on-blue-background-3d-illustration.jpg?b=1&s=170667a&w=0&k=20&c=DkGuYrbbcZc3_zbRZKOq6qhjZ-xNqEkVzU1RIlrabmY="
+                ></Image>
+              </div>
+            </Col>
+            <Col sx={12} sm={12} md={12} lg={12} >
+              <Form onSubmit={(event)=>{event.preventDefault();event.target.reset();}} className="w-50 w-sm-100 m-auto">
+                <FloatingLabel className="mb-2" label="Email">
+                  <Form.Control
+                      name="email"
+                      type="email"
+                      className="border-4 border-dark"
+                      placeholder="Enter email"
+                      required
+                  />
+                </FloatingLabel>
+                <FloatingLabel className="mb-2" label="Phone Number">
+                  <Form.Control
+                      name="phone"
+                      type="phone"
+                      className="border-4 border-dark"
+                      placeholder="Enter phone"
+                      required
+                  />
+                </FloatingLabel>
+
+                  <FloatingLabel className="mb-2" label="Message">
+                    <Form.Control
+                        as="textarea"
+                        name="message"
+                        placeholder="Message"
+                        required
+                        style={{ height: "100px" }}
+                    />
+                  </FloatingLabel>
+
+
+
+                <div className="mb-4">
+                  <Button
+                      variant="primary"
+                      className="border-4 border-dark me-2"
+                      type="submit"
+                  >
+                    Submit
+                  </Button>
+
+                </div>
+              </Form>
+            </Col>
           </Row>
         </Col>
       </Row>
