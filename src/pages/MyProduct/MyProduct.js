6d1e198ec56149f8c244e7ce31a36b85a85df654
Sharmin Moon <sharminmoon0308@gmail.com>
2022-12-22 11:05:50 +0600

    ux improved

--- src/pages/MyProduct/MyProduct.js
+++ src/pages/MyProduct/MyProduct.js
@@ -4,66 +4,66 @@ import axios from "axios";
 import Product from "../Product/Product";
 
 const MyProduct = () => {
-    document.title = "MyProduct";
+  document.title = "MyProduct";
 
-    const [allProducts, setAllProducts] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+  const [allProducts, setAllProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    async function loadProducts() {
-        setIsLoading(true);
-        let token = localStorage.getItem("token");
+  async function loadProducts() {
+    setIsLoading(true);
+    let token = localStorage.getItem("token");
 
-        const url = `${process.env.REACT_APP_SERVER_BASEURL}/products/my-product`;
-        await axios
-            .get(url, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    "content-type": "application/json",
-                },
-            })
-            .then((res) => res.data)
-            .then((data) => {
-                console.log(`My product>`, data.data);
-                setAllProducts(data.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    }
+    const url = `${process.env.REACT_APP_SERVER_BASEURL}/products/my-product`;
+    await axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "content-type": "application/json",
+        },
+      })
+      .then((res) => res.data)
+      .then((data) => {
+        console.log(`My product>`, data.data);
+        setAllProducts(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
 
-    useEffect(() => {
-        loadProducts();
-    }, []);
+  useEffect(() => {
+    loadProducts();
+  }, []);
 
-    return (
-        <div>
-            <Row>
-                <h3 className="text-center text-info fw-bolder">My Product</h3>
-                {isLoading ? (
-                    <div className="text-center">
-                        <Spinner className="" animation="border" variant="danger" />
-                    </div>
-                ) : (
-                    ""
-                )}
-                {allProducts?.map((product, index) => (
-                    <Col sx={12} sm={12} md={6} lg={6}>
-                        <Product
-                            key={index}
-                            product={product}
-                            fromSellerProduct={true}
-                            loadProducts={loadProducts}
-                        >
-                            {" "}
-                        </Product>
-                    </Col>
-                ))}
-            </Row>
-        </div>
-    );
+  return (
+    <div>
+      <Row>
+        <h3 className=" myproduct text-center  fw-bolder">My Product</h3>
+        {isLoading ? (
+          <div className="text-center">
+            <Spinner className="" animation="border" variant="danger" />
+          </div>
+        ) : (
+          ""
+        )}
+        {allProducts?.map((product, index) => (
+          <Col sx={12} sm={12} md={6} lg={6}>
+            <Product
+              key={index}
+              product={product}
+              fromSellerProduct={true}
+              loadProducts={loadProducts}
+            >
+              {" "}
+            </Product>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 };
 
 export default MyProduct;
